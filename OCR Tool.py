00c49bd0c7a8795705c293d07400bcf143b2dfc5
mjscalexander <35542354+mjscalexander@users.noreply.github.com>
2018-03-07 16:32:37 +0000

    Add files via upload

---
+++ OCR Tool.py
@@ -0,0 +1,21 @@
+import pytesseract
+from PIL import Image
+import cv2
+import os
+
+
+image = cv2.imread("2ku.png")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.threshold(gray,0,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+gray = cv2.medianBlur(gray,3)
+filename = '{}.png'.format(os.getpid())
+cv2.imwrite(filename,gray)
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
+text = pytesseract.image_to_string(Image.open(filename))
+os.remove(filename)
+print(text)
+
+
+
+
+
